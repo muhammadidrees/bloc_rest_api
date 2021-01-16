13337f00e87bd30c6bfd9ec34a22e152c31d5744
Muhammad Idrees <midrees097@gmail.com>
2021-01-17 00:06:05 +0500

    removed equatable from package

--- lib/src/cubits/cubits.dart
+++ lib/src/cubits/cubits.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:bloc_rest_api/src/general_response_repository.dart';
 import 'package:bloc_rest_api/src/models/models.dart';
-import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
