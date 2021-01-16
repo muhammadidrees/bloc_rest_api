9d25fce72509d2b7aedadc9ab16918dc1a122549
Muhammad Idrees <midrees097@gmail.com>
2021-01-16 21:53:10 +0500

    updated example to show get request for one model

---
+++ example/lib/models/models.dart
@@ -0,0 +1,3 @@
+import 'package:bloc_rest_api/bloc_rest_api.dart';
+
+part 'post_model.dart';
---
+++ example/lib/models/post_model.dart
@@ -0,0 +1,57 @@
+part of 'models.dart';
+
+// data model must extend the ResultModel class and overide it's methods
+class PostModel extends ResultModel {
+  PostModel({
+    this.userId,
+    this.id,
+    this.title,
+    this.body,
+  });
+
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  PostModel copyWith({
+    int userId,
+    int id,
+    String title,
+    String body,
+  }) =>
+      PostModel(
+        userId: userId ?? this.userId,
+        id: id ?? this.id,
+        title: title ?? this.title,
+        body: body ?? this.body,
+      );
+
+  factory PostModel.fromJson(Map<String, dynamic> json) => PostModel(
+        userId: json["userId"] == null ? null : json["userId"],
+        id: json["id"] == null ? null : json["id"],
+        title: json["title"] == null ? null : json["title"],
+        body: json["body"] == null ? null : json["body"],
+      );
+
+  @override
+  PostModel fromJson(Map<String, dynamic> json) => PostModel(
+        userId: json["userId"] == null ? null : json["userId"],
+        id: json["id"] == null ? null : json["id"],
+        title: json["title"] == null ? null : json["title"],
+        body: json["body"] == null ? null : json["body"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId == null ? null : userId,
+        "id": id == null ? null : id,
+        "title": title == null ? null : title,
+        "body": body == null ? null : body,
+      };
+
+  @override
+  List<Object> get props => [userId, id];
+
+  @override
+  String toString() => "$id -- $title";
+}
