492a14e11e0a1b6eb73df4e2a8ea21509637cb05
Muhammad Idrees <midrees097@gmail.com>
2021-01-17 13:47:26 +0500

    minor updates in example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,7 @@ void main() {
 class App extends StatelessWidget {
   App({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
