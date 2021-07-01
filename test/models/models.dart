ebe6fbfb07314c9f276a6e312030d06d89dda6a2
Muhammad Idrees <midrees097@gmail.com>
2021-07-01 22:27:27 +0500

    made a cubit test working with repository responce

---
+++ test/models/models.dart
@@ -0,0 +1,3 @@
+import 'package:equatable/equatable.dart';
+
+part 'post_model.dart';
---
+++ test/models/post_model.dart
@@ -0,0 +1,90 @@
+part of 'models.dart';
+
+class PostModel extends Equatable {
+  PostModel({
+    this.userId,
+    this.id,
+    this.title,
+    this.body,
+  });
+
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  /// Example json string representation of a single object
+  /// used for testing
+  static String get singlePostResponse => '''
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+  }
+  ''';
+
+  /// Example json string representation of list of objects
+  /// used for testing
+  static String get listPostResponse => '''
+  [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    }
+  ]
+  ''';
+
+  PostModel copyWith({
+    int userId,
+    int id,
+    String title,
+    String body,
+  }) =>
+      PostModel(
+        userId: userId ?? this.userId,
+        id: id ?? this.id,
+        title: title ?? this.title,
+        body: body ?? this.body,
+      );
+
+  factory PostModel.fromJson(Map<String, dynamic> json) => PostModel(
+        userId: json['userId'],
+        id: json['id'],
+        title: json['title'],
+        body: json['body'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'userId': userId,
+        'id': id,
+        'title': title,
+        'body': body,
+      };
+
+  @override
+  List<Object> get props => [userId, id];
+
+  @override
+  String toString() => '$id -- $title';
+}
